d94ab45f00d5fec8beefcb4b7d5c9e4e69045001
Louis GUTEL <louis.gutel@etudiant.univ-lr.fr>
2024-12-13 12:26:51 +0100

    Update index.html to index.ph

---
+++ apropo.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section id="about-section" class="page bg-style1">
+                <!-- Begin page header-->
+                <div class="page-header-wrapper">
+                    <div class="container">
+                        <div class="page-header text-center wow fadeInUp" data-wow-delay="0.3s">
+                            <h2>A propos de moi</h2>
+                            <div class="devider"></div>
+                            <p class="subtitle">Information</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- End page header-->
+
+                <!-- Begin rotate box-1 -->
+                <div class="rotate-box-1-wrapper">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-3 col-sm-6">
+                                <a href="#" class="rotate-box-1 square-icon wow zoomIn" data-wow-delay="0">
+                                    <span class="rotate-box-icon"><i class="fa fa-users"></i></span>
+                                    <div class="rotate-box-info">
+                                        <h4>Qui je suis ?</h4>
+                                        <p>Je suis un étudiant de 3ème année de BUT Réseaux et Télécom avec la 
+                                            spécialisation en cybersécurité à La Rochelle </p>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div class="col-md-3 col-sm-6">
+                                <a href="#" class="rotate-box-1 square-icon wow zoomIn" data-wow-delay="0.2s">
+                                    <span class="rotate-box-icon"><i class="fa fa-diamond"></i></span>
+                                    <div class="rotate-box-info">
+                                        <h4>Qu'est-ce que je fait?</h4>
+                                        <p>À côté de mes études, je fais des projets personnel l'IT, je suis réserviste en gendarmerie et très sportif</p>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div class="col-md-3 col-sm-6">
+                                <a href="#" class="rotate-box-1 square-icon wow zoomIn" data-wow-delay="0.4s">
+                                    <span class="rotate-box-icon"><i class="fa fa-heart"></i></span>
+                                    <div class="rotate-box-info">
+                                        <h4>Pourquoi je fais cette formation?</h4>
+                                        <p>Je fais cette formation car j'aime le réseau et je trouve que un BUT reste dans la continuité de mon Bac technologique. </p>
+                                    </div>
+                                </a>
+                            </div>
+                            
+                            <div class="col-md-3 col-sm-6">
+                                <a href="#" class="rotate-box-1 square-icon wow zoomIn" data-wow-delay="0.6s">
+                                    <span class="rotate-box-icon"><i class="fa fa-clock-o"></i></span>
+                                    <div class="rotate-box-info">
+                                        <h4>Depuis quand ?</h4>
+                                        <p>J'ai cette passion qui c'est développé depuis mon stage de 3ème dans l'instalation informatique. Suite à plusieurs piratage je m'intéresse au dommaine de la cybersécurité</p>
+                                    </div>
+                                </a>
+                            </div>
+                            
+                        </div> <!-- /.row -->
+                    </div> <!-- /.container -->
+                </div>
+                <!-- End rotate box-1 -->
+                
+                <div class="extra-space-l"></div>
+            </section>
+          <!-- End about section -->
+</body>
+</html>
